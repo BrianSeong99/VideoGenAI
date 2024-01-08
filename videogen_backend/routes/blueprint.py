dc7f745976abd0d2d93edf2a102b155aa93b973b
JinwooSeong <brianseong.dev@gmail.com>
2024-01-08 14:59:42 -0800

    WIP: webhook configuration

--- videogen_backend/routes/blueprint.py
+++ videogen_backend/routes/blueprint.py
@@ -3,6 +3,6 @@ from controller.controller import upload_video, cloudinary_webhook
 
 blueprint = Blueprint('blueprint', __name__)
 
-blueprint.route('/cloudinary_webhook', methods=['GET'])(cloudinary_webhook)
+blueprint.route('/cloudinary_webhook', methods=['POST'])(cloudinary_webhook)
 blueprint.route('/upload', methods=['POST'])(upload_video)
 # blueprint.route('/upload_multiple', methods=['POST'])(upload_videos)
