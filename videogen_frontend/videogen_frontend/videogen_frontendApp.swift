441f0bd87c4a98813c607c3438c7cf66c4ec8393
JinwooSeong <brianseong.dev@gmail.com>
2024-01-09 01:21:58 -0800

    feat: frontend initial commit

---
+++ videogen_frontend/videogen_frontend/videogen_frontendApp.swift
@@ -0,0 +1,20 @@
+//
+//  videogen_frontendApp.swift
+//  videogen_frontend
+//
+//  Created by JIN WOO SEONG on 2024/1/9.
+//
+
+import SwiftUI
+
+@main
+struct videogen_frontendApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
